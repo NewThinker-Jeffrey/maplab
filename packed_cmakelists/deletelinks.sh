0bcde74ece261cb46f31f6ef56a203eca4806eef
Jiwey <jiwey@newthinker.net>
2018-08-10 13:56:46 +0800

    add packed_cmakelists

---
+++ packed_cmakelists/deletelinks.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+my_loc="$(cd "$(dirname $0)" && pwd)"
+
+## maplab_core
+module="maplab_core/algorithms"
+cmd="rm ${my_loc}/${module}/code"
+echo $cmd
+$cmd
+
+
+module="maplab_core/common"
+cmd="rm ${my_loc}/${module}/code"
+echo $cmd
+$cmd
+
+module="maplab_core/aslam_cv2"
+cmd="rm ${my_loc}/${module}/code"
+echo $cmd
+$cmd
+
+
+module="maplab_core/map-structure"
+cmd="rm ${my_loc}/${module}/code"
+echo $cmd
+$cmd
+
+
+module="maplab_core/backend"
+cmd="rm ${my_loc}/${module}/code"
+echo $cmd
+$cmd
+
+
+## console-plugins
+module="console-plugins"
+cmd="rm ${my_loc}/${module}/code"
+echo $cmd
+$cmd
+
+
