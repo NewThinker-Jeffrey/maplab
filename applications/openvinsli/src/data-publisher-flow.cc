fe52bcc05b8073f5a5dce120f44bea8b872f1809
Jeffrey <jeffrey@newthinker.net>
2024-09-18 11:26:17 +0800

    [openvinsli] publish the pose of realsense_link frame to tf

--- applications/openvinsli/src/data-publisher-flow.cc
+++ applications/openvinsli/src/data-publisher-flow.cc
@@ -665,8 +665,34 @@ void DataPublisherFlow::publishVinsState(
     eigenMatrixToOdometryCovariance(
         vinode.getPoseCovariance(), odom_T_M_I.pose.covariance.data());
   }
+
+  aslam::Transformation T_I_Curdf;
+  std::string tf_urdf_cam_frame = "realsense_link";
+  {
+    // - [1.0, 0.0, 0.0, -0.03022]
+    // - [0.0, 1.0, 0.0,  0.0074 ]
+    // - [0.0, 0.0, 1.0,  0.01602]
+
+    Eigen::Isometry3d pose_M_I(T_M_I.getRotation().toImplementation());
+    pose_M_I.translation() = T_M_I.getPosition();
+    auto Zc = pose_M_I.rotation().col(2);
+    float yaw = atan2(Zc.y(), Zc.x());
+    pose_M_I.linear() = Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ()).toRotationMatrix();
+    // pose_M_I.translation().z() = 0.0;  // Fix z = 0?
+
+    Eigen::Matrix3d R_I_Cleft;
+    R_I_ Cleft << 1, 0, 0,
+                  0, 1, 0,
+                  0, 0, 1;
+    Eigen::Vector3d T_I_Curdf(-0.03022, 0.0074, 0.01602);
+
+    T_I_Curdf = aslam::Transformation(T_I_Curdf, Eigen::Quaterniond(R_I_Cleft));
+  }
+
   visualization::publishTF(
       T_M_I, FLAGS_tf_mission_frame, FLAGS_tf_imu_frame, timestamp_ros);
+  visualization::publishTF(
+      T_M_I * T_I_Curdf, FLAGS_tf_mission_frame, tf_urdf_cam_frame, timestamp_ros);
 
   // Publish pose in global frame.
   aslam::Transformation T_G_I = T_G_M * T_M_I;
