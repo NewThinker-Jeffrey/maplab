11375b674577c241a13c2fc6e21c5fcd2f60bd80
Jeffrey <jeffrey@newthinker.net>
2024-12-04 12:26:15 +0800

    [mininav2d] add mininav2d-fastlio-loc-ros.cc

--- applications/mininav2d/app/mininav2d-fastlio-loc-ros.cc
+++ applications/mininav2d/app/mininav2d-fastlio-loc-ros.cc
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/Odometry.h>
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_datatypes.h>
 
@@ -16,35 +17,19 @@
 #include "message-flow/message-dispatcher-fifo.h"
 
 
-DEFINE_string(input_pose_topic, "input_pose", "input_pose_topic");
+DEFINE_string(fastlio_odom_topic, "/Odometry", "fastlio_odom_topic");
+DEFINE_string(fastlio_reloc_topic, "/map_to_odom", "fastlio_reloc_topic");
 
 DEFINE_string(tf_mission_frame, "odom", "tf_mission_frame");
 DEFINE_string(tf_map_frame, "map", "tf_map_frame");
 DEFINE_string(tf_imu_frame, "imu", "tf_imu_frame");
 DEFINE_string(tf_urdf_cam_frame, "head_rs_link", "tf_urdf_cam_frame");
 
-// std::shared_ptr<mininav2d::OpenvinsliNode> openvins_localization_node = nullptr;
-
-// __sighandler_t old_sigint_handler = nullptr;
-// void shutdownSigintHandler(int sig) {
-//   std::cout << "[APP STATUS] Stop Requested ... " << std::endl;
-//   if (openvins_localization_node) {
-//     openvins_localization_node->shutdown();
-//   }
-//   // Keep ros alive to finish the map visualization.
-
-//   // ros::shutdown();
-
-//   // if (old_sigint_handler) {
-//   //   old_sigint_handler(sig);
-//   // }
-// }
-
 
 static std::unique_ptr<ros::NodeHandle> nh;  ///, nh_private;
-static std::unique_ptr<ros::Subscriber> pose_subscriber;
+static std::unique_ptr<ros::Subscriber> fastlio_odom_subscriber;
+static std::unique_ptr<ros::Subscriber> fastlio_reloc_subscriber;
 static std::unique_ptr<ros::Publisher> publish_global_pose_to_ros;
-
 
 // nav2d related
 using StampedGlobalPose = mininav2d::StampedGlobalPose;
@@ -53,40 +38,74 @@ static std::unique_ptr<message_flow::MessageFlow> flow;
 static std::function<void(const StampedGlobalPose::ConstPtr&)> publish_global_pose_to_msgflow;
 static std::unique_ptr<mininav2d::Nav2dFlow> nav2d;
 
-static void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
-{
-  double pose_x = msg->pose.position.x;
-  double pose_y = msg->pose.position.y;
-  double pose_z = msg->pose.position.z;
-  double pose_qx = msg->pose.orientation.x;
-  double pose_qy = msg->pose.orientation.y;
-  double pose_qz = msg->pose.orientation.z;
-  double pose_qw = msg->pose.orientation.w;
+static std::mutex mutex_T_map_odom;
+static Pose3d T_map_odom;
+static bool T_map_odom_valid = false;
 
+static Pose3d fromRosPose(const geometry_msgs::Pose& ros_pose) {
+  double pose_x =  ros_pose.position.x;
+  double pose_y =  ros_pose.position.y;
+  double pose_z =  ros_pose.position.z;
+  double pose_qx = ros_pose.orientation.x;
+  double pose_qy = ros_pose.orientation.y;
+  double pose_qz = ros_pose.orientation.z;
+  double pose_qw = ros_pose.orientation.w;
   Eigen::Vector3d position(pose_x, pose_y, pose_z);
   Eigen::Quaterniond orientation(pose_qw, pose_qx, pose_qy, pose_qz);
-  Pose3d transform(orientation.toRotationMatrix(), position);
+  return Pose3d(orientation.toRotationMatrix(), position);
+}
 
-  auto stamped_pose = std::make_shared<mininav2d::StampedGlobalPose>();
+static geometry_msgs::Pose toRosPose(const Pose3d& pose) {
+  geometry_msgs::Pose ros_pose;
+  Eigen::Quaterniond q(pose.linear().matrix());
+  Eigen::Vector3d t = pose.translation();
+  ros_pose.position.x = t.x();
+  ros_pose.position.y = t.y();
+  ros_pose.position.z = t.z();
+  ros_pose.orientation.x = q.x();
+  ros_pose.orientation.y = q.y();
+  ros_pose.orientation.z = q.z();
+  ros_pose.orientation.w = q.w();
+  return ros_pose;
+}
+
+static void fastlioOdomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+  // get odom pose from msg
+  auto stamped_pose = std::make_shared<StampedGlobalPose>();
   stamped_pose->timestamp_ns = msg->header.stamp.toNSec();
-  stamped_pose->odom_pose = transform;
-  stamped_pose->global_pose = transform;
-  stamped_pose->global_pose_valid = true;
+  stamped_pose->odom_pose = fromRosPose(msg->pose.pose);
+  stamped_pose->global_pose_valid = false;
+  {
+    std::unique_lock lock(mutex_T_map_odom);
+    if (T_map_odom_valid) {
+      stamped_pose->global_pose = T_map_odom * stamped_pose->odom_pose;
+      stamped_pose->global_pose_valid = true;
+    }
+  }
+
   if (publish_global_pose_to_msgflow) {
     publish_global_pose_to_msgflow(stamped_pose);  // send to message flow
   }
 
-  // forward to new pose topic
-  geometry_msgs::PoseStamped new_pose_msg;
-  new_pose_msg.header = msg->header;
-  new_pose_msg.pose.position.x = pose_x;
-  new_pose_msg.pose.position.y = pose_y;
-  new_pose_msg.pose.position.z = pose_z;
-  new_pose_msg.pose.orientation.x = pose_qx;
-  new_pose_msg.pose.orientation.y = pose_qy;
-  new_pose_msg.pose.orientation.z = pose_qz;
-  new_pose_msg.pose.orientation.w = pose_qw;
-  publish_global_pose_to_ros->publish(new_pose_msg);
+  if (stamped_pose->global_pose_valid && publish_global_pose_to_ros) {
+    // forward to new pose topic
+    geometry_msgs::PoseStamped new_pose_msg;
+    new_pose_msg.header = msg->header;
+    new_pose_msg.pose = toRosPose(stamped_pose->global_pose);
+    publish_global_pose_to_ros->publish(new_pose_msg);
+  }
+}
+
+static void fastlioRelocCallback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+  // get map_to_odom from msg
+  Pose3d map_to_odom = fromRosPose(msg->pose.pose);
+  {
+    std::unique_lock lock(mutex_T_map_odom);
+    T_map_odom = map_to_odom;
+    T_map_odom_valid = true;
+  }
 }
 
 int main(int argc, char** argv) {
@@ -119,8 +138,10 @@ int main(int argc, char** argv) {
   // // This must be set after the first NodeHandle is created.
   // old_sigint_handler = signal(SIGINT, shutdownSigintHandler);
 
-  pose_subscriber.reset(new ros::Subscriber(
-      nh->subscribe(FLAGS_input_pose_topic, 1, poseCallback)));
+  fastlio_odom_subscriber.reset(new ros::Subscriber(
+      nh->subscribe(FLAGS_fastlio_odom_topic, 1, fastlioOdomCallback)));
+  fastlio_reloc_subscriber.reset(new ros::Subscriber(
+      nh->subscribe(FLAGS_fastlio_reloc_topic, 1, fastlioRelocCallback)));
 
   ros::spin();
 
